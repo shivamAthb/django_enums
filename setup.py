d5ea6efa59f58f49fe6737baa6527ae0dcc3649c
Shivam Sharma <shivam.sharma@helixbeat.com>
2025-08-17 21:49:04 +0530

    #1 updated setup

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="django_enums",
+    name="dj_standard_enums",
     version="0.0.1",
     packages=find_packages(),
     include_package_data=True,
