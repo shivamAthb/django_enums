6a6de8cb3fff4971da93550b45cbd75367244a67
Shivam Sharma <shivam.sharma@helixbeat.com>
2025-09-09 17:01:15 +0530

    added filters on extra_data

--- enums/apiviews.py
+++ enums/apiviews.py
@@ -11,15 +11,30 @@ class RetrieveEnumAPIView(APIView):
     permission_classes = [AllowAny]  # Add permission classes if needed
 
     @staticmethod
-    def get_enum_representation(enum_class_name):
+    def get_enum_representation(enum_class_name, filters=None):
         registry = SystemEnumRegistry()
         enum_class = registry.get_enum_class(class_name=enum_class_name)
         if enum_class is None:
             return []
 
-        return enum_class.get_full_representation()
+        full_representation = enum_class.get_full_representation()
+        if not filters:
+            return full_representation
+        filtered_representation = []
+        for item in full_representation:
+            extra_data, matching = item.get("extra_data", {}), True
+            for filter_key, filter_value in filters.items():
+                if extra_data.get(filter_key) not in filter_value:
+                    matching = False
+                    break
+            if matching:
+                filtered_representation.append(item)
+        return filtered_representation
 
     def get(self, request, *args, **kwargs):
         enum_class_name = kwargs.get("enum_class_name")
-        data = self.get_enum_representation(enum_class_name=enum_class_name)
+        filters = dict(self.request.query_params)
+        data = self.get_enum_representation(
+            enum_class_name=enum_class_name, filters=filters
+        )
         return Response(data=data, status=status.HTTP_200_OK)
--- enums/constants.py
+++ enums/constants.py
@@ -3,5 +3,5 @@ from enums.framework.item import StandardEnumItem
 
 
 class TestNewEnum(StandardEnum):
-    PASSED = StandardEnumItem(code="PASSED")
-    FAILED = StandardEnumItem(code="FAILED")
+    PASSED = StandardEnumItem(code="PASSED", extra_data={"category": "success"})
+    FAILED = StandardEnumItem(code="FAILED", extra_data={"category": "danger"})
